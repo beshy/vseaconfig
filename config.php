f5baddf7ff7620a6120f63be83720c32c33e71e7
beShy <i@beshy.net>
2013-09-25 16:10:56 +0800

    diff reveal merge

--- config.php
+++ config.php
@@ -13,6 +13,8 @@ $C['API_REVEAL'] = $C['API_URL'].'app/reveal/?u=';
 
 $C['OTT_API'] = 'http://60.170.244.9:8090/ott/play/ott';
 
+$C['MERGE_URL'] = 'http://'.$C['ROOT_DN'].'/app/reveal/?u=';
+
 foreach($C as $k => $v) {
 	if (!defined($k))
 		define($k, $v);
