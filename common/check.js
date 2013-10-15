10e19021c98b0edc26a755e1eec1a7d7b2192d2b
beShy <i@beshy.net>
2013-10-15 16:55:53 +0800

    fix iqiyi

--- common/check.js
+++ common/check.js
@@ -144,7 +144,7 @@
 		}
 	} else if ( null != (m=url.match(/.*m\.iqiyi\.com\/play.html.*?tvid\=([^\&]+).*?vid\=([^\&]+)/i)) ) {
 		
-		ext += '&iid='+m[1]+'/'+m[2];
+		ext += '&iid='+m[1]+'_'+m[2];
 		if (window.tvInfoJs) {
 			data.img = window.tvInfoJs.vpic;
 			data.title = window.tvInfoJs.vn;
