3326da15c3639acaa76ed6c436fd9e123ff7613f
beShy <i@beshy.net>
2013-12-03 18:36:40 +0800

    tudou remove iid check

--- c_common.php
+++ c_common.php
@@ -7,16 +7,16 @@ $p=array(
 // ---------------------------------------------------------
 
 	array(
-		'url' => '^(http\:\/\/)(m\.tv\.sohu\.com\/.*|v\.youku\.com\/v_show\/id_.*|www\.tudou\.com\/.+|m\.iqiyi\.com\/.+|m\.56\.com\/.+|m\.letv\.com\/vplay.+|m\.ku6\.com\/show\/.+|v\.ifeng\.com\/.+|m\.pptv\.com\/show\/.+|m\.pps\.tv\/play.+)$',
+		'url' => '^([^\/]*?\:\/\/)(m\.tv\.sohu\.com\/.*|v\.youku\.com\/v_show\/id_.*|www\.tudou\.com\/.+|m\.iqiyi\.com\/.+|m\.56\.com\/.+|m\.letv\.com\/vplay.+|m\.ku6\.com\/show\/.+|v\.ifeng\.com\/.+|m\.pptv\.com\/show\/.+|m\.pps\.tv\/play.+|[^\/]+\/app\/yinyuetai\/movie.+|[^\/]+\/app\/soku.+)$',
 		'tpl' => 'common/check',
 		'type' => '1',
 	),
 
-	array(
-		'url' => '^[^\/]*?\:\/\/[^\/]+\/app\/(vqq|yinyuetai\/movie|ifengTV).*$',
-		'tpl' => 'common/blank',
-		'type' => '1',
-	),
+	//array(
+	//	'url' => '^[^\/]*?\:\/\/[^\/]+\/app\/(vqq|yinyuetai\/movie|ifengTV).*$',
+	//	'tpl' => 'common/blank',
+	//	'type' => '1',
+	//),
 
 
 // ---------------------------------------------------------
