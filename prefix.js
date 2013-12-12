c35fbb82c9720ce31cbe0fa8e92f46aa5e73fb27
beShy <i@beshy.net>
2013-12-12 16:50:21 +0800

    fix netip

--- prefix.js
+++ prefix.js
@@ -1,5 +1,5 @@
 ;
-window.PLAY_URL = (window.netip && window.netip!='')? 'http://'+window.netip+':8080/app/reveal/?u=' : '<?php echo PLAY_URL;?>';
-window.MERGE_URL = (window.netip && window.netip!='')? 'http://'+window.netip+':8080/app/reveal/?u=' : '<?php echo MERGE_URL;?>';
+window.PLAY_URL = (window.netip && window.netip!='')? 'http://'+window.netip+'/app/reveal/?u=' : '<?php echo PLAY_URL;?>';
+window.MERGE_URL = (window.netip && window.netip!='')? 'http://'+window.netip+'/app/reveal/?u=' : '<?php echo MERGE_URL;?>';
 window.OTT_API = '';
 window.SHOW_TAB = 'no';
