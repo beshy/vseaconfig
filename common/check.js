1dd19fdecf5ab7f34b006af0555cfe3d4256722d
beShy <i@beshy.net>
2014-01-03 09:51:08 +0800

    fixbug

--- common/check.js
+++ common/check.js
@@ -18,7 +18,7 @@
 	console.log('init check')
 	var url = (window.__src_url) ? window.__src_url : window.location.href;
 	var body = (window.__src_code) ? window.__src_code : document.documentElement.outerHTML;
-	var m = null, m2 = null;
+	var m = null, m2 = null, isTimeout = false;
 	var src = null;
 
 	var img, title;
@@ -68,69 +68,66 @@
 		if ('' == data.param) {
 			return ;
 		}
+		console.log('getAdNum');
 		adp = adp || '';
-		parseComplete++;
 		data.param += '&adp='+adp
 		var url = MERGE_API+data.param+'&mode=getOrigSrcs&ptype=ad&rtype=json&callback=__getAdNum';
-		insertScript(url);
+		insertScript(url, true);
+	};
+
+	var setUrls = function (url) {
+		src = getRevealUrl(url);
+		data.param = getParam(url);
+
+		data.m_url = src[0]+'&quality=0';
+
+		var mu = [src[1]+'&quality=0', src[1]+'&quality=1', src[1]+'&quality=2'];
+		data.ottsd_url= mu[1];
+		data.otthd_url= mu[2];
 	};
 
 
 	var returnPageDataDone = false;
-	var returnPageData = function (force) {
-		if (returnPageDataDone || parseComplete>0)
+	var returnPageData = function () {
+		if (returnPageDataDone) {
 			return;
-		if ( force || (data && data.img && data.m_url) ) {
-			returnPageDataDone = true;
-			if ( !(data && data.m_url) ) {
-				console.log('get page data null');
-				return;
-			}
+		}
+
+		if ( parseComplete>0 ) {
+			return setTimeout(returnPageData, 300);
+		}
+
+		returnPageDataDone = true;
+		if ( data && data.title && data.m_url ) {
 			data.title = escape(data.title);
-			var s = JSON.stringify(data);
-			console.log('get page data: '+s);
-			if (window.sendOTTData) {
-				console.log('call sendOTTData.send');
-				window.sendOTTData.send(s);
-			} else {
-				console.log('no sendOTTData method.');
-			}
-		}
-	};
-
-	var simpleAjax = function (url, sf, ef, df) {
-		var xhr;
-		if (window.XMLHttpRequest){
-			xhr=new XMLHttpRequest();
 		} else {
-			xhr=new ActiveXObject("Microsoft.XMLHTTP");
-		}
-		xhr.onreadystatechange = function(){
-			if(xhr.readyState === 4){
-				if(xhr.status === 200){
-					if (sf) {
-						sf(xhr.responseText);
-					}
-				} else {
-					if (ef) {
-						ef(xhr.status);
-					}
-				}
-
-				if (df) {
-					df(xhr.status);
-				}
-			}
+			data = null;
 		}
 
-		xhr.open("GET",url,true);
-		xhr.send();
+		var s = JSON.stringify(data);
+		console.log('get page data: '+s);
+		if (window.sendOTTData) {
+			console.log('call sendOTTData.send');
+			window.sendOTTData.send(s);
+		} else {
+			console.log('no sendOTTData method.');
+		}
+
 	};
 
-	var insertScript = function (url) {
+	var insertScript = function (url, ito, to) {
+		if (undefined === ito) {
+			ito = isTimeout;
+		}
+		console.log('insertScript: '+url);
+		to = to || 5000;
 		var e=document.createElement('script'); 
 		e.setAttribute('src', url); 
 		document.head.appendChild(e);
+		parseStart();
+		if (ito) {
+			setTimeout(parseDone, to);
+		}
 	};
 
 	var JSONparse = function (s) {
@@ -149,24 +146,16 @@
 
 
 	var parseComplete = 0;
+	var parseStart = function () {
+		parseComplete++;
+		console.log('parseStart');
+	};
 	var parseDone = function () {
 		parseComplete--;
-		if (parseComplete<1) {
-			returnPageData();
-		}
+		console.log('parseDone');
+		returnPageData();
 	};
 
-	window.__getAdLocation = function (d) {
-		data.ad_url = d.location;
-		parseDone();
-	};
-
-	var getAdLocation = function (u) {
-		parseComplete++;
-		var urls = getRevealUrl(encodeURI(u));
-		u = urls[1]+'&rtype=json&callback=__getAdLocation';
-		insertScript(u);
-	};
 
 	// http://v.youku.com/v_show/id_XNTkyNjY1NjQ0.html?f=19532522&ev=1
 	if ( null != (m=url.match(/^http\:\/\/v\.youku\.com\/v_show\/id_([^\&\#\/\.]*).*$/i)) ) {
@@ -174,8 +163,7 @@
 		var vid=m[1];
 
 		var youkuUrl = "http://v.youku.com/v_show/id_"+vid+".html"
-		src=getRevealUrl(youkuUrl);
-		data.param = getParam(youkuUrl);
+		setUrls(youkuUrl);
 
 		var __youku_complete = 0;
 		window.__check_getYoukuData = function (d) {
@@ -190,8 +178,6 @@
 			parseDone();
 		};
 
-
-		parseComplete++;
 		insertScript('http://v.youku.com/player/getPlaylist/VideoIDS/'+vid+'/Pf/4?__callback=__check_getYoukuData');
 
 		if (window.videoId) {
@@ -201,14 +187,10 @@
 			getAdNum(showId+'_'+videoId+'_'+videoOwnerID);
 		}
 
-
-		setTimeout(parseDone, 5000);
-
 	} else if ( null != (m=url.match(/tv.sohu.com/i)) && null != (m=body.match(/\s+vid\s*[\:\=]\s*\"(\d+)\"/i)) && null != (m2=body.match(/og\:url.*?content\=\"(.+?)\"/i)) ) {
 		// sohu
 		ext += '&iid='+m[1];
-		src=getRevealUrl(m2[1]);
-		data.param = getParam(m2[1]);
+		setUrls(m2[1]);
 
 		if ( null != (img=body.match(/apple-touch-icon-precomposed.*?href=\"(.*?)\"/i)) && null != (title=body.match(/keywords.*?content=\"(.*?)\"/i)) ) {
 			data.img = img[1];
@@ -218,8 +200,24 @@
 		if (window.VideoData && window.VideoData.vid && window.VideoData.tvid && window.VideoData.sid) {			
 			getAdNum(VideoData.sid+'_'+VideoData.vid+'_'+VideoData.tvid+'_'+VideoData.duration);
 		}
+
+		var lastVid = false;
+		var checkChange = function () {
+			if (window.VideoData) {
+				var v = window.VideoData;
+				if (!lastVid) {
+					lastVid = v.vid;
+					return;
+				}
+				if (v.vid != lastVid) {
+					console.log('get new location');
+					clearInterval(checkChangeI);
+					window.location.href = "http://m.tv.sohu.com/v"+v.vid+".shtml";
+				}
+			}
+		};
+		var checkChangeI=setInterval(checkChange, 50);
 		
-		setTimeout(parseDone, 5000);
 	} else if ( null != (m=url.match(/.*tudou.com.*/i)) && window.itemData ) {
 		var _d = window.itemData;
 		if (_d.vcode && _d.vcode!='') {
@@ -227,8 +225,7 @@
 		} else {
 			//ext += '&iid='+_d.iid;
 		}
-		src = getRevealUrl(url);
-		data.param = getParam(url);
+		setUrls(url);
 
 		data.img = _d.pic;
 		data.title = _d.kw;
@@ -255,11 +252,6 @@
 		};
 		var checkChangeI=setInterval(checkChange, 50);
 
-		// check ad
-		// if (window.itemData && window.itemData.iid && window.itemData.oid && window.aid) {
-		// 	getAdLocation('http://valf.atm.youku.com/vf?vl=256&ct=a&cs=2148&td=0'+'&s='+aid+'&v='+itemData.iid+'&u='+itemData.oid);
-		// }
-
 		var checkTudouAd = function () {
 			if (window.itemData && window.itemData.iid) {
 				var showId = window.aid ? window.aid : '0';
@@ -274,27 +266,23 @@
 		};
 
 		if ( !checkTudouAd() ) {
-			parseComplete++;
+			parseStart();
 			setTimeout(function () {
-
 				checkTudouAd();
 				parseDone();
 			}, 500);
 		}
 		
-		setTimeout(parseDone, 5000);
 
 	} else if ( null != (m=url.match(/.*letv.com.*/i)) && (
 		null != (m=url.match(/m\.letv\.com\/vplay\_(.*?)\.html.*/i)) ||
 		null != (m=url.match(/www\.letv\.com\/ptv\/vplay\/(.*?)\.html.*/i))
 		) ) {
 		var leurl = 'http://www.letv.com/ptv/vplay/'+m[1]+'.html';
-		src = getRevealUrl(leurl);
-		data.param = getParam(leurl);
+		setUrls(leurl);
 		
 
 		getAdNum(m[1]);
-		setTimeout(parseDone, 5000);
 
 		console.log('check letv');
 
@@ -309,49 +297,48 @@
 
 	} else if ( null != (m=url.match(/.*sina.cn.*/i)) && null != (m=body.match(/location\.php\?.*?url\=([^\&\#\'\"]+)/i)) ) {
 		var u = decodeURIComponent(m[1]);
-		src = getRevealUrl(u);
-		data.param = getParam(u);
+		setUrls(u);
 		if ( null != (img=body.match(/poster=\"(.*?)\"/i)) && null != (title=body.match(/<h2>(.*?)<\/h2>/i)) ) {
 			data.img = img[1];
 			data.title = title[1];
 		}
 	} else if ( null != (m=url.match(/.*56.com.*/i)) && null != (m=body.match(/src\=\"http\:\/\/vxml.56.com\/html5\/(\d+)\//i)) ) {
 		ext += '&iid='+m[1];
-		src = getRevealUrl(url);
-		data.param = getParam(url);
+		setUrls(url);
 		if ( null != (img=body.match(/url\((.*?)\)/i)) && null != (title=body.match(/keywords.*?content=\"(.*?)\"/i)) ) {
 			data.img = img[1];
 			data.title = title[1];
 		}
 	//} else if ( null != (m=url.match(/.*\.iqiyi\.com\/play.html.*?tvid\=([^\&\#]+).*?vid\=([^\&\#]+)/i)) ) {
-	} else if ( null != (m=url.match(/.*\.iqiyi\.com/i)) ) {
+	} else if ( null != (m=url.match(/.*\.iqiyi\.com/i)) || null != (m=url.match(/.*\.iqiyi\.com\/play.html.*?tvid\=([^\&\#]+).*?vid\=([^\&\#]+)/i)) ) {
 		//ext += '&iid='+m[1]+'_'+m[2];
-		src = getRevealUrl(url);
-		data.param = getParam(url);
+		setUrls(url);
 
 		var lastVid = '';
-		if (window.Q.PageInfo) {
+
+		if (window.Q.PageInfo && Q.PageInfo.playInfo && Q.PageInfo.playInfo.vn) {
 			data.img = Q.PageInfo.playInfo.vpic;
 			data.title = Q.PageInfo.playInfo.vn;
 			lastVid = Q.PageInfo.playInfo.vid;
 			getAdNum();
-			setTimeout(parseDone, 5000);
+		} else {
+			parseStart();
 		}
 
 		var checkChange = function () {
 			if (window.tvInfoJs) {
 				var info = window.tvInfoJs;
-				if (!returnPageDataDone) {
-					data.img = info.vpic;
-					data.title = info.vn;
-					returnPageData();
-				}
-
+				
 				if (!lastVid) {
 					lastVid = info.vid;
+					data.img = info.vpic;
+					data.title = info.vn;
+					setUrls(info.vu);
+					getAdNum();
+					parseDone();
 					return;
 				}
-				
+
 				if (info.vid != lastVid) {
 					console.log('iqiyi get new location')
 					clearInterval(checkChangeI);
@@ -360,20 +347,19 @@
 				}
 			}
 		};
+
 		var checkChangeI=setInterval(checkChange, 50);
 
 
 	} else if ( null != (m=url.match(/.*m.(ku6.com.*)/i)) ) {
-		src = getRevealUrl('http://v.'+m[1]);
-		data.param = getParam('http://v.'+m[1]);
+		setUrls('http://v.'+m[1]);
 		if ( null != (img=body.match(/vid,\s*\'(.*?)\'/i)) && null != (title=body.match(/<title>(.*?)<\/title>/i)) ) {
 			data.img = img[1];
 			data.title = title[1];
 		}
 	} else if ( null != (m=url.match(/.*v\.ifeng\.com.*/i)) && (null != (m=url.match(/([\w\d]+\-[\w\d]+\-[\w\d]+\-[\w\d]+\-[\w\d]+)/i)) || null != (m=body.match(/([\w\d]+\-[\w\d]+\-[\w\d]+\-[\w\d]+\-[\w\d]+)/i)) ) ) {
 		ext += '&iid='+m[1];
-		src = getRevealUrl(url);
-		data.param = getParam(url);
+		setUrls(url);
 		if ( null != (img=body.match(/image\"\s*content\=\"(.*?)\"/i)) && null != (title=body.match(/title\"\s*content\=\"(.*?)\"/i)) ) {
 			data.img = img[1];
 			data.title = title[1];
@@ -391,19 +377,16 @@
 		})();
 	} else if ( null != (m=url.match(/.*[mv].(pptv.com.*)/i)) && window.m_info && window.webcfg ) {
 		ext += '&iid=' + webcfg.channel_id;
-		src = getRevealUrl('http://v.'+m[1]);
-		data.param = getParam('http://v.'+m[1]);
+		setUrls('http://v.'+m[1]);
 		
 		data.img = 'http://s'+rnd(1, 4)+'.pplive.cn/v/cap/'+webcfg.channel_id+'/h160.jpg';
 		data.title = m_info.title;
 
 		getAdNum();
-		setTimeout(parseDone, 5000);
 
 	} else if ( null != (m=url.match(/.*?\.(pps\.tv\/play.*)/i)) ) {
 		//ext += '&iid=' + page_var.url_key;
-		src = getRevealUrl('http://v.'+m[1]);
-		data.param = getParam('http://v.'+m[1]);
+		setUrls('http://v.'+m[1]);
 		if ( window.page_var ) {
 			data.img = page_var.video_img;
 			data.title = page_var.file_title;
@@ -418,22 +401,21 @@
 		
 
 		getAdNum();
-		setTimeout(parseDone, 5000);
 
 	} else if ( null != (m=url.match(/.*?app\/yinyuetai\/movie.*/i)) && window.__PAGE_DATA ) {
 		ext += '&iid=' + __PAGE_DATA.iid;
-		src = getRevealUrl(__PAGE_DATA.url);
-		data.param = getParam(__PAGE_DATA.url);
+		setUrls(__PAGE_DATA.url);
 		data.title = __PAGE_DATA.title;
 		data.img = __PAGE_DATA.img;
+
 	} else if ( null != (m=url.match(/.*?yinyuetai.com\/video\/.*/i)) ) {
-		src = getRevealUrl(url);
-		data.param = getParam(url);
+		setUrls(url);
 		
 		if ( null != (title=body.match(/title\"\s*content=\"(.*?)\"/i)) && null != (img=body.match(/image\"\s*content=\"(.*?)\"/i)) ) {
 			data.img = img[1];
 			data.title = title[1];
 		}
+
 	} else if ( null != (m=url.match(/.*?mv\.yinyuetai\.com.*/i)) ) {
 		var setPlayVideoHere = function () {
 			window.playVideoHere = function (t, id) {
@@ -472,8 +454,7 @@
 		checkPlay();
 	} else if ( null != (m=url.match(/.*?yinyuetai.com\/wap\/video\/(\d+).*/i)) ) {
 		var yurl = 'http://v.yinyuetai.com/video/'+m[1];
-		src = getRevealUrl(yurl);
-		data.param = getParam(yurl);
+		setUrls(yurl);
 		
 		if ( null != (title=body.match(/<h1[^<>]*?title[^<>]*?>([\w\W]*?)<\/h1>/i)) && null != (img=body.match(/<img[^<>]*?src\=\"(.*?)\"[^<>]*?>/i)) ) {
 			data.img = img[1];
@@ -482,8 +463,7 @@
 	} else if ( null != (m=url.match(/.*?yinyuetai.com\/wap\/video\/(\d+).*/i)) || null != (m=body.match(/data-videoid\=\"(\d+)\"/i)) ) {
 		
 		var yurl = 'http://v.yinyuetai.com/video/'+m[1];
-		src = getRevealUrl(yurl);
-		data.param = getParam(yurl);
+		setUrls(yurl);
 		
 		
 		if ( null != (title=body.match(/<h1[^<>]*?title[^<>]*?>([\w\W]*?)<\/h1>/i)) && null != (img=body.match(/<img[^<>]*?src\=\"(.*?)\"[^<>]*?>/i)) ) {
@@ -492,28 +472,10 @@
 		}
 	}
 
-
 	// check hidePlay button
 	if  (window.hidePlayButton && window.SHOW_TAB=='no') {
 		window.hidePlayButton();
 	}
-
-	if ( null != src && '' != src ) {
-		var appendBtn = function () {
-			data.m_url = src[0]+'&quality=0';
-
-			var mu = [src[1]+'&quality=0', src[1]+'&quality=1', src[1]+'&quality=2'];
-			data.ottsd_url= mu[1];
-			data.otthd_url= mu[2];
-			returnPageData();
-		};
-
-		appendBtn();
-	} else {
-		data = null;
-		returnPageData(true);
-	}
-
 
 	returnPageData();
 })();
